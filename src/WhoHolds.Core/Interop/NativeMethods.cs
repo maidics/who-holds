023720ef9b0434b14ccf25221d59b59938b58b3e
maidics <your-username@users.noreply.github.com>
2026-09-09 10:52:56 +0200

    feat: add NtQuerySystemInformation method

---
+++ src/WhoHolds.Core/Interop/NativeMethods.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WhoHolds.Core.Interop;
+
+internal static class NativeMethods
+{
+    private const string NtDll = "ntdll.dll";
+
+    [DllImport(NtDll, EntryPoint = "NtQuerySystemInformation")]
+    internal static extern uint GetNtSystemInfo(int systemInformationClass, IntPtr systemInformation, int systemInformationLength, out int returnLength);
+}
