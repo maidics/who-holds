db65e0ea6b10e6e09eb5f67a65813027193e48cc
maidics <your-username@users.noreply.github.com>
2026-09-09 14:45:56 +0200

    feat: add NativeBuffer

---
+++ src/WhoHolds.Core/Interop/NativeBuffer.cs
@@ -0,0 +1,36 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WhoHolds.Core.Interop;
+
+internal sealed class NativeBuffer : IDisposable
+{
+    private IntPtr _pointer;
+
+    public NativeBuffer(int size)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThanOrEqual(size, 0);
+
+        Size = size;
+        _pointer = Marshal.AllocHGlobal(size);
+    }
+
+    public IntPtr Pointer
+    {
+        get
+        {
+            ObjectDisposedException.ThrowIf(_pointer == IntPtr.Zero, this);
+            return _pointer;
+        }
+    }
+
+    public int Size { get; }
+
+    public void Dispose()
+    {
+        if (_pointer != IntPtr.Zero)
+        {
+            Marshal.FreeHGlobal(Pointer);
+            _pointer = IntPtr.Zero;
+        }
+    }
+}
