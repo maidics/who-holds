d42dbc91d37172247fc2a3e66d084b74c3a69518
maidics <your-username@users.noreply.github.com>
2026-09-09 14:51:42 +0200

    feat: add NtException

---
+++ src/WhoHolds.Core/Interop/Exceptions/NtException.cs
@@ -0,0 +1,15 @@
+﻿using WhoHolds.Core.Interop.Enums;
+
+namespace WhoHolds.Core.Interop.Exceptions;
+
+internal sealed class NtException : Exception
+{
+    private NtException(string message)
+        : base(message) { }
+
+    public static void ThrowIfInfoLengthMismatch(NtStatus status, SystemInformationClass cls)
+    {
+        if (status is NtStatus.InfoLengthMismatch)
+            throw new NtException($"Buffer too small for {cls}");
+    }
+}
