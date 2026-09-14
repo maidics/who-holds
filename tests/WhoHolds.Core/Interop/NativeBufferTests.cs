4105e8cd48ecefdd9ccb1acd854a7f22a71ea7ba
maidics <your-username@users.noreply.github.com>
2026-09-14 11:27:58 +0200

    test: refactor and extended NativeBufferTests for cls

--- tests/WhoHolds.Core/Interop/NativeBufferTests.cs
+++ tests/WhoHolds.Core/Interop/NativeBufferTests.cs
@@ -1,6 +1,7 @@
 ﻿using System.Runtime.InteropServices;
 using Shouldly;
 using WhoHolds.Core.Interop;
+using WhoHolds.Core.Interop.Enums;
 
 namespace WhoHolds.Core.Tests.Interop;
 
@@ -11,13 +12,15 @@ public sealed class NativeBufferTests
     [Arguments(0)]
     public void ShouldThrowIfSizeIsNotPositive(int size)
     {
-        Should.Throw<ArgumentOutOfRangeException>(() => new NativeBuffer(size));
+        Should.Throw<ArgumentOutOfRangeException>(() =>
+            new NativeBuffer(size, SystemInformationClass.Basic)
+        );
     }
 
     [Test]
     public void DisposeShouldBeIdempotent()
     {
-        var buffer = new NativeBuffer(10);
+        var buffer = new NativeBuffer(10, SystemInformationClass.Basic);
 
         buffer.Dispose();
         Should.NotThrow(buffer.Dispose);
@@ -28,19 +31,24 @@ public sealed class NativeBufferTests
     public void ShouldProvideWritableMemoryForTheFullSize()
     {
         const int size = 4096;
-        using var buffer = new NativeBuffer(size);
+        const SystemInformationClass systemInformationClass = SystemInformationClass.Basic;
+        using var buffer = new NativeBuffer(size, systemInformationClass);
 
         for (int i = 0; i < size; i++)
             Marshal.WriteByte(buffer.Pointer, i, (byte)(i % 251));
 
         for (int i = 0; i < size; i++)
             Marshal.ReadByte(buffer.Pointer, i).ShouldBe((byte)(i % 251));
+
+        buffer.Pointer.ShouldNotBe(IntPtr.Zero);
+        buffer.Size.ShouldBe(size);
+        buffer.SystemInformationClass.ShouldBe(systemInformationClass);
     }
 
     [Test]
     public void ShouldAllocateOnInstantiationAndReleaseOnDispose()
     {
-        var buffer = new NativeBuffer(10);
+        var buffer = new NativeBuffer(10, SystemInformationClass.Basic);
 
         buffer.Size.ShouldBe(10);
         buffer.Pointer.ShouldNotBe(IntPtr.Zero);
