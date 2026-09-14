2a52c2840300ea72b812c73f121768979a4c2a71
maidics <your-username@users.noreply.github.com>
2026-09-14 11:13:33 +0200

    feat: add ObjectTypes

---
+++ src/WhoHolds.Core/Interop/Structs/ObjectTypeInformation.cs
@@ -0,0 +1,48 @@
+﻿using System.Runtime.InteropServices;
+
+namespace WhoHolds.Core.Interop.Structs;
+
+[StructLayout(LayoutKind.Sequential)]
+struct ObjectTypeInformation
+{
+    public UnicodeString TypeName;
+    public uint TotalNumberOfObjects,
+        TotalNumberOfHandles;
+    public uint TotalPagedPoolUsage,
+        TotalNonPagedPoolUsage,
+        TotalNamePoolUsage,
+        TotalHandleTableUsage;
+    public uint HighWaterNumberOfObjects,
+        HighWaterNumberOfHandles;
+    public uint HighWaterPagedPoolUsage,
+        HighWaterNonPagedPoolUsage,
+        HighWaterNamePoolUsage,
+        HighWaterHandleTableUsage;
+    public uint InvalidAttributes;
+    public GenericMapping GenericMapping;
+    public uint ValidAccessMask;
+    public byte SecurityRequired,
+        MaintainHandleCount;
+    public byte TypeIndex; // valid on Windows 8.1+
+    public byte ReservedByte;
+    public uint PoolType,
+        DefaultPagedPoolCharge,
+        DefaultNonPagedPoolCharge;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct UnicodeString
+{
+    public ushort Length,
+        MaximumLength;
+    public IntPtr Buffer;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+struct GenericMapping
+{
+    public uint R,
+        W,
+        E,
+        A;
+}
