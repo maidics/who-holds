1b492bd9d20caf542299342863518277cb5c6296
maidics <your-username@users.noreply.github.com>
2026-09-14 09:10:08 +0200

    fix: property name typo

--- src/WhoHolds.Core/Interop/Structs/SystemHandleEntryEx.cs
+++ src/WhoHolds.Core/Interop/Structs/SystemHandleEntryEx.cs
@@ -8,7 +8,7 @@ internal struct SystemHandleEntryEx
 {
     public IntPtr Object;
     public IntPtr UniqueProcessId;
-    public IntPtr HandleValues;
+    public IntPtr HandleValue;
     public uint GrantedAccess;
     public ushort CreatorBackTraceIndex;
     public ushort ObjectTypeIndex;
@@ -17,6 +17,6 @@ internal struct SystemHandleEntryEx
 
     public override string ToString()
     {
-        return $"{nameof(SystemHandleInformationEx)}\n\t- Object: 0x{Object:X}\n\t- UniqueProcessId: 0x{UniqueProcessId:X}\n\t- HandleValues: 0x{HandleValues:X}\n\t- GrantedAccess: {GrantedAccess}\n\t - CreatorBackTraceIndex: {CreatorBackTraceIndex}\n\t - ObjectTypeIndex: {ObjectTypeIndex}\n\t - Reserved: {Reserved}";
+        return $"{nameof(SystemHandleInformationEx)}\n\t- Object: 0x{Object:X}\n\t- UniqueProcessId: 0x{UniqueProcessId:X}\n\t- HandleValue: 0x{HandleValue:X}\n\t- GrantedAccess: {GrantedAccess}\n\t - CreatorBackTraceIndex: {CreatorBackTraceIndex}\n\t - ObjectTypeIndex: {ObjectTypeIndex}\n\t - Reserved: {Reserved}";
     }
 }
