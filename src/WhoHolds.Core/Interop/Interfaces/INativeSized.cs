bf25bd9091e394724b44ec8edc995e9bc3a0982b
maidics <your-username@users.noreply.github.com>
2026-09-16 15:35:45 +0200

    feat: rename and add unmanaged constraint to INativeSized implementation

---
+++ src/WhoHolds.Core/Interop/Interfaces/INativeSized.cs
@@ -0,0 +1,7 @@
+﻿namespace WhoHolds.Core.Interop.Interfaces;
+
+internal interface INativeSized<TSelf>
+    where TSelf : unmanaged, INativeSized<TSelf> // constraints implementation
+{
+    static abstract int Size { get; } // used for testing and guards
+}
