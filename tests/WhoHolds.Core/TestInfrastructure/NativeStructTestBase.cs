ba69e36b496a4d4e4651d6244e203a7636d186e8
maidics <your-username@users.noreply.github.com>
2026-09-16 15:36:16 +0200

    refactor: NativeStructTestBase to take size in constructor

---
+++ tests/WhoHolds.Core/TestInfrastructure/NativeStructTestBase.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using WhoHolds.Core.Interop.Interfaces;
+
+namespace WhoHolds.Core.Tests.TestInfrastructure;
+
+internal abstract class NativeStructTestBase<TStruct>(int expectedSize)
+    where TStruct : unmanaged, INativeSized<TStruct>
+{
+    [Test]
+    public void ShouldHaveSequentialLayout()
+    {
+        var layout = typeof(TStruct).StructLayoutAttribute;
+        layout.ShouldNotBeNull();
+        layout.Value.ShouldBe(LayoutKind.Sequential);
+    }
+
+    [Test]
+    public void ShouldHaveCorrectSize()
+    {
+        TStruct.Size.ShouldBe(expectedSize);
+    }
+
+    protected static void AssertInlineArrayAttributeAndArrayType(int length, Type arrayType)
+    {
+        var structType = typeof(TStruct);
+
+        var inlineArray = structType.GetCustomAttribute<InlineArrayAttribute>();
+        inlineArray.ShouldNotBeNull();
+        inlineArray.Length.ShouldBe(length);
+
+        var fields = structType.GetFields(
+            BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic
+        );
+        fields.Length.ShouldBe(1);
+
+        var field = fields[0];
+        field.FieldType.ShouldBe(arrayType);
+    }
+}
