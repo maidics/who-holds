8b1ff49b7df3ea93adf555d30c6bceb3fbc77720
maidics <your-username@users.noreply.github.com>
2026-09-16 16:15:17 +0200

    test: add declaration tests to RmGetList and RmRegisterResources

---
+++ tests/WhoHolds.Core/Interop/NativeMethods/RmGetListTests.cs
@@ -0,0 +1,56 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using WhoHolds.Core.Interop.Enums;
+using WhoHolds.Core.Interop.Structs;
+using WhoHolds.Core.Tests.TestInfrastructure;
+
+namespace WhoHolds.Core.Tests.Interop.NativeMethods;
+
+internal sealed class RmGetListTests()
+    : NativeMethodTestBase(nameof(Core.Interop.NativeMethods.RmGetList))
+{
+    [Test]
+    public override void ShouldBeDeclaredCorrectly()
+    {
+        var parameters = AssertMethodDeclaration(
+            typeof(SystemErrorCode),
+            [
+                typeof(uint),
+                typeof(uint).MakeByRefType(),
+                typeof(uint).MakeByRefType(),
+                typeof(RmProcessInfo[]),
+                typeof(RmRebootReason).MakeByRefType(),
+            ],
+            MethodAttributes.Static
+        );
+
+        var pnProcInfoNeeded = parameters[1];
+        pnProcInfoNeeded.IsIn.ShouldBeFalse();
+        pnProcInfoNeeded.IsOut.ShouldBeTrue();
+
+        var pnProcInfo = parameters[2];
+        pnProcInfo.ParameterType.IsByRef.ShouldBeTrue(); // ref uint pnProcInfo
+
+        var pnRebootReason = parameters[4];
+        pnRebootReason.IsIn.ShouldBeFalse();
+        pnRebootReason.IsOut.ShouldBeTrue();
+    }
+
+    [Test]
+    public override void ShouldBeDecoratedWithImportAttribute()
+    {
+        var libraryImport = _methodInfo.GetCustomAttribute<LibraryImportAttribute>();
+        libraryImport.ShouldNotBeNull();
+        libraryImport.LibraryName.ShouldBe("rstrtmgr.dll");
+        libraryImport.StringMarshalling.ShouldBe(StringMarshalling.Utf16);
+    }
+
+    [Test]
+    public void ShouldBeDecoratedWithDefaultDllImportSearchPathsAttribute()
+    {
+        var defaultDllImportSearchPaths =
+            _methodInfo.GetCustomAttribute<DefaultDllImportSearchPathsAttribute>();
+        defaultDllImportSearchPaths.ShouldNotBeNull();
+        defaultDllImportSearchPaths.Paths.ShouldBe(DllImportSearchPath.System32);
+    }
+}
---
+++ tests/WhoHolds.Core/Interop/NativeMethods/RmRegisterResourcesTests.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using WhoHolds.Core.Interop.Enums;
+using WhoHolds.Core.Interop.Structs;
+using WhoHolds.Core.Tests.TestInfrastructure;
+
+namespace WhoHolds.Core.Tests.Interop.NativeMethods;
+
+internal sealed class RmRegisterResourcesTests()
+    : NativeMethodTestBase(nameof(Core.Interop.NativeMethods.RmRegisterResources))
+{
+    [Test]
+    public override void ShouldBeDeclaredCorrectly()
+    {
+        AssertMethodDeclaration(
+            typeof(SystemErrorCode),
+            [
+                typeof(uint),
+                typeof(uint),
+                typeof(string[]) /* nullable reference types are compile time annotations only */
+                ,
+                typeof(uint),
+                typeof(RmUniqueProcess[]),
+                typeof(uint),
+                typeof(string[]),
+            ],
+            MethodAttributes.Static
+        );
+    }
+
+    [Test]
+    public override void ShouldBeDecoratedWithImportAttribute()
+    {
+        var libraryImport = _methodInfo.GetCustomAttribute<LibraryImportAttribute>();
+        libraryImport.ShouldNotBeNull();
+        libraryImport.LibraryName.ShouldBe("rstrtmgr.dll");
+        libraryImport.StringMarshalling.ShouldBe(StringMarshalling.Utf16);
+    }
+
+    [Test]
+    public void ShouldBeDecoratedWithDefaultDllImportSearchPathsAttribute()
+    {
+        var defaultDllImportSearchPaths =
+            _methodInfo.GetCustomAttribute<DefaultDllImportSearchPathsAttribute>();
+        defaultDllImportSearchPaths.ShouldNotBeNull();
+        defaultDllImportSearchPaths.Paths.ShouldBe(DllImportSearchPath.System32);
+    }
+
+    // TODO: add error code tests
+}
