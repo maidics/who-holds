39b9842d02f7c86ce072f6f1d7155981f8a4402b
maidics <your-username@users.noreply.github.com>
2026-09-17 11:46:30 +0200

    feat: add IRmFunctionReference interface for error handling

---
+++ src/WhoHolds.Core/Interop/Interfaces/IRmFunctionReference.cs
@@ -0,0 +1,30 @@
+﻿using WhoHolds.Core.Common.Models;
+using WhoHolds.Core.Interop.Enums;
+
+namespace WhoHolds.Core.Interop.Interfaces;
+
+public interface IRmFunctionReference
+{
+    static abstract string FunctionName { get; }
+    static abstract string DocumentationLink { get; }
+    static abstract string GetErrorMessageByErrorCode(SystemErrorCode code);
+
+    static abstract Result GetResultByErrorCode(SystemErrorCode code);
+
+    static string[] GetResultErrorMessages(
+        SystemErrorCode code,
+        string documentationLink,
+        params string[] errors
+    ) =>
+        [
+            .. errors,
+            GetSystemErrorCodeMessage(code),
+            GetDocumentationErrorMessage(documentationLink),
+        ];
+
+    static string GetSystemErrorCodeMessage(SystemErrorCode code) =>
+        $"{code.ToString()} ({(uint)code})";
+
+    static string GetDocumentationErrorMessage(string documentationLink) =>
+        $"More information about this function: {documentationLink}";
+}
