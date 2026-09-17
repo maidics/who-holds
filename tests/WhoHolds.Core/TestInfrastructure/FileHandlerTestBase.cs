696a1be83ca4e93657a189092bb3428768265172
maidics <your-username@users.noreply.github.com>
2026-09-17 16:37:26 +0200

    test: base class for tests that require files

---
+++ tests/WhoHolds.Core/TestInfrastructure/FileHandlerTestBase.cs
@@ -0,0 +1,30 @@
+﻿namespace WhoHolds.Core.Tests.TestInfrastructure;
+
+internal abstract class FileHandlerTestBase
+{
+    private static string _tempDir = null!;
+
+    [Before(Class)]
+    public static void SetUp()
+    {
+        _tempDir = Directory.CreateTempSubdirectory("rm-tests-").FullName;
+    }
+
+    [After(Class)]
+    public static void TearDown()
+    {
+        try
+        {
+            Directory.Delete(_tempDir, recursive: true);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(
+                $"Failed to delete temp test folder: {ex.Message} at \"{_tempDir}\"."
+            );
+        }
+    }
+
+    protected static string CreateTestFile(string? name = null) =>
+        Path.Combine(_tempDir, name ?? Guid.NewGuid().ToString());
+}
