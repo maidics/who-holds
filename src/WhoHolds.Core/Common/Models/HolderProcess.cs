f657f6034fdae9d9439be0a31d054732a2510a8f
maidics <your-username@users.noreply.github.com>
2026-09-18 12:33:20 +0200

    feat: add HolderProcess to return to CLI

---
+++ src/WhoHolds.Core/Common/Models/HolderProcess.cs
@@ -0,0 +1,13 @@
+﻿namespace WhoHolds.Core.Common.Models;
+
+//TODO: add a command that describes the fields with sample values
+public sealed record HolderProcess(
+    int ProcessId, // .NET ProcessId is an int
+    DateTime StartTime, // UTC time
+    string? ApplicationName,
+    string? ServiceShortName, // only set for services
+    string ApplicationType, // interop enum
+    List<string> ApplicationStatus, // interop flag enum => descibes history of actions taken by Restart Manager on the application
+    int? SessionId, // session can be unknown (-1) => null
+    bool Restartable
+);
