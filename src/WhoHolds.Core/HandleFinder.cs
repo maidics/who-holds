7781552005e48b95e27249497437af97687be64a
maidics <your-username@users.noreply.github.com>
2026-09-18 14:27:58 +0200

    feat: add HandleFinder public API

---
+++ src/WhoHolds.Core/HandleFinder.cs
@@ -0,0 +1,28 @@
+﻿using WhoHolds.Core.Common.Models;
+using WhoHolds.Core.Interop;
+using WhoHolds.Core.Utility;
+
+namespace WhoHolds.Core;
+
+public static class HandleFinder
+{
+    public static Result<HolderProcess[]> GetHolderProcessesToFile(string file)
+    {
+        var fileCheckResult = PathUtils.CheckFilePath(file);
+
+        if (!fileCheckResult.Succeeded)
+            return fileCheckResult.ToFailure<HolderProcess[]>();
+
+        using var session = new RestartManagerSession([file]);
+        session.Start();
+
+        var getResult = session.GetProcesses(out _);
+
+        if (!getResult.Succeeded)
+            return getResult.ToFailure<HolderProcess[]>();
+
+        var processes = getResult.Value.Select(p => p.ToHolderProcess()).ToArray();
+
+        return Result.Success(processes);
+    }
+}
