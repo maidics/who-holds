c1a2b3a4848cb692a893997d42cef5f71a5f915f
maidics <your-username@users.noreply.github.com>
2026-09-18 09:15:00 +0200

    test: add RestartManagerSessionTests

---
+++ tests/WhoHolds.Core/Interop/RestartManagerSessionTests.cs
@@ -0,0 +1,138 @@
+﻿using System.Reflection;
+using WhoHolds.Core.Interop;
+using WhoHolds.Core.Tests.TestInfrastructure;
+
+namespace WhoHolds.Core.Tests.Interop;
+
+internal sealed class RestartManagerSessionTests : FileHandlerTestBase
+{
+    [Test]
+    public void ConstructorShouldThrowArgumentOutOfRangeExceptionIf0FilePathsPassed()
+    {
+        Should.Throw<ArgumentOutOfRangeException>(() => new RestartManagerSession([]));
+    }
+
+    [Test]
+    public void ConstructorShouldThrowIfOneOrMoreFilePathsAreInvalid()
+    {
+        var invalidFile1 = Guid.NewGuid().ToString();
+        var invalidFile2 = Guid.NewGuid().ToString();
+
+        string[] files = [CreateTestFile(), invalidFile1, invalidFile2];
+
+        var ex = Should.Throw<ArgumentException>(() => new RestartManagerSession(files));
+
+        ex.Message.ShouldNotContain(files[0]);
+        ex.Message.ShouldContain(invalidFile1);
+        ex.Message.ShouldContain(invalidFile2);
+    }
+
+    [Test]
+    public void ShouldCreateSession()
+    {
+        string[] files = [CreateTestFile(), CreateTestFile()];
+
+        Should.NotThrow(() => new RestartManagerSession(files));
+    }
+
+    [Test]
+    public void ShouldStartSessionAndDisposeIt()
+    {
+        string[] files = [CreateTestFile()];
+
+        var session = new RestartManagerSession(files);
+
+        var result = session.Start();
+        result.Succeeded.ShouldBeTrue();
+
+        var startedField = typeof(RestartManagerSession).GetField(
+            "_started",
+            BindingFlags.Instance | BindingFlags.NonPublic
+        );
+        startedField.ShouldNotBeNull();
+        var started = (bool)startedField.GetValue(session).ShouldNotBeNull();
+        started.ShouldBeTrue();
+
+        session.Dispose();
+
+        var disposedField = typeof(RestartManagerSession).GetField(
+            "_disposed",
+            BindingFlags.Instance | BindingFlags.NonPublic
+        );
+        disposedField.ShouldNotBeNull();
+        var disposed = (bool)disposedField.GetValue(session).ShouldNotBeNull();
+        disposed.ShouldBeTrue();
+    }
+
+    [Test]
+    public void StartMethodShouldThrowIfDisposed()
+    {
+        string[] files = [CreateTestFile()];
+
+        var session = new RestartManagerSession(files);
+        session.Dispose();
+
+        Should.Throw<ObjectDisposedException>(() => session.Start());
+    }
+
+    [Test]
+    public void StartMethodShouldThrowIfAlreadyStarted()
+    {
+        string[] files = [CreateTestFile()];
+
+        using var session = new RestartManagerSession(files);
+        var result = session.Start();
+        result.Succeeded.ShouldBeTrue();
+
+        Should.Throw<InvalidOperationException>(() => session.Start());
+    }
+
+    [Test]
+    public void DisposeShouldBeIdempotent()
+    {
+        string[] files = [CreateTestFile()];
+
+        var session = new RestartManagerSession(files);
+        session.Dispose();
+
+        Should.NotThrow(() => session.Dispose());
+        Should.NotThrow(() => session.Dispose());
+        Should.NotThrow(() => session.Dispose());
+    }
+
+    [Test]
+    public void GetProcessesShouldThrowIfSessionIsDisposed()
+    {
+        string[] files = [CreateTestFile()];
+
+        var session = new RestartManagerSession(files);
+        session.Dispose();
+
+        Should.Throw<ObjectDisposedException>(() => session.GetProcesses(out _));
+    }
+
+    [Test]
+    public void GetProcessesShouldReturnHolderProcessesForFiles()
+    {
+        var heldFile = CreateTestFile();
+        var nonHeldFile = CreateTestFile();
+
+        string[] files = [heldFile, nonHeldFile];
+        using var hold = new FileStream(
+            heldFile,
+            FileMode.Open,
+            FileAccess.ReadWrite,
+            FileShare.None
+        );
+
+        using var session = new RestartManagerSession(files);
+        session.Start();
+
+        var result = session.GetProcesses(out _);
+        result.Succeeded.ShouldBeTrue();
+
+        var processes = result.Value;
+        processes.Length.ShouldBe(1);
+        processes.ShouldContain(p => p.Process.dwProcessId == Environment.ProcessId);
+    }
+}
