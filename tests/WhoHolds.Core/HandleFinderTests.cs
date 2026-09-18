b5014ab1a2a00628f3e8ed0af943206086202780
maidics <your-username@users.noreply.github.com>
2026-09-18 14:29:24 +0200

    test: add HandleFinderTests

---
+++ tests/WhoHolds.Core/HandleFinderTests.cs
@@ -0,0 +1,40 @@
+﻿using WhoHolds.Core.Common.Models;
+using WhoHolds.Core.Tests.TestInfrastructure;
+
+namespace WhoHolds.Core.Tests;
+
+internal sealed class HandleFinderTests : PathHandlerTestBase
+{
+    [Test] // since this uses PathUtils covering one case is enough
+    public void GetHolderProcessesToFileShouldReturnNotFoundIfFileNotFound()
+    {
+        var result = HandleFinder.GetHolderProcessesToFile(
+            Path.Combine(_tempDir, Guid.NewGuid().ToString())
+        );
+        result.Type.ShouldBe(ResultType.NotFound);
+    }
+
+    [Test]
+    public void GetHolderProcessesToFileShouldReturnEmptyArrayIfNothingHoldsFile()
+    {
+        var file = CreateTestFile();
+
+        var result = HandleFinder.GetHolderProcessesToFile(file);
+        result.ShouldBeResultedTo(ResultType.Success);
+        result.Value.Length.ShouldBe(0);
+    }
+
+    [Test] // because it orchestrates the search with RestartManagerSession covering one case where it returns something is enough as well
+    public void GetHolderProcessesToFileShouldReturnHolderProcesses()
+    {
+        var file = CreateTestFile();
+        using var hold = HoldFile(file);
+
+        var result = HandleFinder.GetHolderProcessesToFile(file);
+        result.ShouldBeResultedTo(ResultType.Success);
+
+        var processes = result.Value;
+        processes.Length.ShouldBe(1);
+        processes[0].ProcessId.ShouldBe(Environment.ProcessId);
+    }
+}
