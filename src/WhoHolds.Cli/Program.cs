dd1a75c5d9c1eca0a502f8315dede2b4dcf2a057
maidics <your-username@users.noreply.github.com>
2026-09-21 10:13:03 +0200

    feat: add root command to Program.cs

--- src/WhoHolds.Cli/Program.cs
+++ src/WhoHolds.Cli/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using WhoHolds.Cli.Commands;
+using WhoHolds.Cli.Output;
+
+var root = new GetFileHoldersCommand(Console.Out, Console.Error);
+var parseResult = root.Parse(args);
+
+if (parseResult.Errors.Count > 0)
+{
+    parseResult.Invoke();
+    return ExitCodes.Error;
+}
+
+return parseResult.Invoke();
