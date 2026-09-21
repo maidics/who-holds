086265aed8000d29f773b4f63a3e628d497c1ed2
maidics <your-username@users.noreply.github.com>
2026-09-21 14:42:19 +0200

    feat: add stderr to IResultWriter.Write

--- src/WhoHolds.Cli/Output/TextResultWriter.cs
+++ src/WhoHolds.Cli/Output/TextResultWriter.cs
@@ -4,13 +4,13 @@ namespace WhoHolds.Cli.Output;
 
 public sealed class TextResultWriter : IResultWriter
 {
-    public void Write(Result<HolderProcess[]> result, TextWriter output)
+    public void Write(Result<HolderProcess[]> result, TextWriter stdout, TextWriter stderr)
     {
         if (!result.Succeeded)
         {
             foreach (var error in result.Errors)
             {
-                output.WriteLine(error);
+                stderr.WriteLine(error);
             }
 
             return;
@@ -18,13 +18,13 @@ public sealed class TextResultWriter : IResultWriter
 
         if (result.Value.Length == 0)
         {
-            output.WriteLine("No processes are holding the file.");
+            stdout.WriteLine("No processes are holding the file.");
             return;
         }
 
         foreach (var holderProcess in result.Value)
         {
-            output.WriteLine(holderProcess.ToString());
+            stdout.WriteLine(holderProcess.ToString());
         }
     }
 }
