9fa438d17fa80d5ed09274bbf4ecc3f650a4a5a7
maidics <your-username@users.noreply.github.com>
2026-09-22 09:57:50 +0200

    ci: add Pipelines project

---
+++ src/WhoHolds.Pipeline/Program.cs
@@ -0,0 +1,17 @@
+﻿using ModularPipelines;
+using ModularPipelines.Extensions;
+using ModularPipelines.Options;
+
+var builder = Pipeline.CreateBuilder(args);
+
+builder.ConfigurePipelineOptions(options =>
+{
+    options.PrintLogo = false;
+    options.ShowProgressInConsole = true;
+    options.ExecutionMode = ExecutionMode.StopOnFirstException;
+});
+
+// TODO: add modules
+
+await using var pipeline = await builder.BuildAsync();
+await pipeline.RunAsync();
