e687f42ad8e989f1a4800cf5a65fc2eb795e26cc
maidics <your-username@users.noreply.github.com>
2026-09-22 13:06:55 +0200

    ci: add RestoreModule and BuildModule

---
+++ src/WhoHolds.Pipeline/Modules/BuildModule.cs
@@ -0,0 +1,26 @@
+﻿using ModularPipelines.Context;
+using ModularPipelines.DotNet.Extensions;
+using ModularPipelines.DotNet.Options;
+using ModularPipelines.Modules;
+using WhoHolds.Pipeline.Constants;
+
+namespace WhoHolds.Pipeline.Modules;
+
+public sealed class BuildModule : Module
+{
+    protected override async Task ExecuteModuleAsync(
+        IModuleContext context,
+        CancellationToken cancellationToken
+    )
+    {
+        var options = new DotNetBuildOptions
+        {
+            NoRestore = true,
+            Nologo = true,
+            ProjectSolution = Repo.Solution,
+            Configuration = Repo.BuildConfiguration,
+        };
+
+        await context.DotNet().Build(options, cancellationToken: cancellationToken);
+    }
+}
