ca7b4259c43a16920bcb42cd2557abdacb8e6daf
maidics <your-username@users.noreply.github.com>
2026-09-22 13:08:51 +0200

    ci: add CiPipeline
    So registrations on builder are testable.

--- src/WhoHolds.Pipeline/Program.cs
+++ src/WhoHolds.Pipeline/Program.cs
@@ -1,17 +1,8 @@
-﻿using ModularPipelines;
-using ModularPipelines.Extensions;
-using ModularPipelines.Options;
+﻿using WhoHolds.Pipeline;
 
 var builder = Pipeline.CreateBuilder(args);
 
-builder.ConfigurePipelineOptions(options =>
-{
-    options.PrintLogo = false;
-    options.ShowProgressInConsole = true;
-    options.ExecutionMode = ExecutionMode.StopOnFirstException;
-});
-
-// TODO: add modules
+CiPipeline.AddCi(builder);
 
 await using var pipeline = await builder.BuildAsync();
 await pipeline.RunAsync();
