0556888611cd5d6535c4efc988ef68d449098061
maidics <your-username@users.noreply.github.com>
2026-09-23 14:34:20 +0200

    chore: add get-release-build-version.ps1

---
+++ scripts/get-release-build-version.ps1
@@ -0,0 +1,5 @@
+﻿(Get-Item artifacts\bin\WhoHolds.Cli\release_win-x64\wh.dll).VersionInfo.ProductVersion
+(Get-Item artifacts\bin\WhoHolds.Cli\release_win-x64\wh.dll).VersionInfo.FileVersion
+
+# AssemblyVersion
+[System.Reflection.AssemblyName]::GetAssemblyName("$PWD\artifacts\bin\WhoHolds.Cli\release_win-x64\wh.dll").Version.ToString()
