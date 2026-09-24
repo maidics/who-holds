653efb1abe57abe4114608eb76418450daec04ee
maidics <your-username@users.noreply.github.com>
2026-09-24 15:47:55 +0200

    ci: add c++ tool locator

--- src/WhoHolds.Pipeline/Constants/Repo.cs
+++ src/WhoHolds.Pipeline/Constants/Repo.cs
@@ -5,4 +5,6 @@ public static class Repo
     public const string Solution = "WhoHolds.slnx";
 
     public const string Configuration = "Release";
+
+    public const string VsWhere = "vswhere.exe";
 }
