124fb75ccaf03abff4bcd2ba261880473df79531
maidics <your-username@users.noreply.github.com>
2026-09-24 15:14:50 +0200

    refactor(ci): add PipelineBuilderExtensions

---
+++ tests/WhoHolds.Pipeline.Tests/Extensions/PipelineBuilderExtensionsTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.Extensions.Configuration.Json;
+using Microsoft.Extensions.DependencyInjection;
+using ModularPipelines;
+using ModularPipelines.Modules;
+using ModularPipelines.Options;
+using ModularPipelines.Requirements;
+using WhoHolds.Pipeline.Extensions;
+using WhoHolds.Pipeline.Modules;
+
+namespace WhoHolds.Pipeline.Tests.Extensions;
+
+public sealed class PipelineBuilderExtensionTests
+{
+    private readonly PipelineBuilder _builder = ModularPipelines.Pipeline.CreateBuilder();
+
+    [Test]
+    public void ShouldConfigurePipeline()
+    {
+        _builder.ConfigurePipeline();
+
+        _builder.Options.PrintLogo.ShouldBeFalse();
+        _builder.Options.ShowProgressInConsole.ShouldBeTrue();
+        _builder.Options.ExecutionMode.ShouldBe(ExecutionMode.StopOnFirstException);
+    }
+
+    [Test]
+    public void ShouldAddJsonConfiguration()
+    {
+        Environment.SetEnvironmentVariable("DOTNET_ENVIRONMENT", "Testing"); // _builder does not respect .Environment.EnvironmentName
+
+        _builder.AddJsonConfiguration();
+
+        var json = _builder.Configuration.Sources.OfType<JsonConfigurationSource>().Single();
+        json.Path.ShouldBe("appsettings.Testing.json");
+        json.Optional.ShouldBeFalse();
+    }
+
+    [Test]
+    public void ShouldAddRequirements()
+    {
+        _builder.AddRequirements();
+
+        _builder.Services.Count(d => d.ServiceType == typeof(IPipelineRequirement)).ShouldBe(1);
+
+        ContainsRequirement<WindowsRequirement>().ShouldBeTrue();
+    }
+
+    [Test]
+    public async Task ShouldAddModules()
+    {
+        _builder.AddModules();
+
+        List<Type> expected = [typeof(RestoreModule), typeof(BuildModule), typeof(TestModule)];
+
+        await using var pipeline = await _builder.BuildAsync();
+
+        pipeline.Services.GetServices<IModule>().Select(m => m.GetType()).ShouldBe(expected);
+    }
+
+    private bool ContainsRequirement<TRequirement>()
+        where TRequirement : IPipelineRequirement =>
+        _builder.Services.Any(d =>
+            d.ServiceType == typeof(IPipelineRequirement)
+            && (d.ImplementationType ?? d.ImplementationInstance?.GetType()) == typeof(TRequirement)
+        );
+}
