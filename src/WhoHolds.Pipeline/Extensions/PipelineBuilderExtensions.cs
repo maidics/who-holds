e7dcecc66a348f90c76af471f06bfa317ef55f45
Maidics Dávid <dmaidics03@gmail.com>
2026-09-24 15:45:46 +0200

    Refactor(ci)/use pipeline builder extensions (#30)
    * ci: add appsettings files
    
    * refactor(ci): add PipelineBuilderExtensions
    
    * test(ci): set DOTNET_ENVIRONMENT before tests and restore it after

---
+++ src/WhoHolds.Pipeline/Extensions/PipelineBuilderExtensions.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using ModularPipelines;
+using ModularPipelines.Extensions;
+using ModularPipelines.Options;
+using ModularPipelines.Requirements;
+using WhoHolds.Pipeline.Modules;
+
+namespace WhoHolds.Pipeline.Extensions;
+
+public static class PipelineBuilderExtensions
+{
+    extension(PipelineBuilder builder)
+    {
+        public PipelineBuilder ConfigurePipeline()
+        {
+            return builder.ConfigurePipelineOptions(options =>
+            {
+                options.PrintLogo = false;
+                options.ShowProgressInConsole = true;
+                options.ExecutionMode = ExecutionMode.StopOnFirstException;
+            });
+        }
+
+        public PipelineBuilder AddJsonConfiguration()
+        {
+            builder.Configuration.AddJsonFile(
+                $"appsettings.{builder.Environment.EnvironmentName}.json",
+                optional: false
+            );
+
+            return builder;
+        }
+
+        public PipelineBuilder AddRequirements()
+        {
+            return builder.AddRequirement<WindowsRequirement>();
+        }
+
+        public PipelineBuilder AddModules()
+        {
+            return builder
+                .AddModule<RestoreModule>()
+                .AddModule<BuildModule>()
+                .AddModule<TestModule>();
+        }
+    }
+}
--- src/WhoHolds.Pipeline/Program.cs
+++ src/WhoHolds.Pipeline/Program.cs
@@ -1,9 +1,9 @@
 ﻿using ModularPipelines;
-using WhoHolds.Pipeline;
+using WhoHolds.Pipeline.Extensions;
 
 var builder = Pipeline.CreateBuilder(args);
 
-CiPipeline.AddCi(builder);
+builder.AddJsonConfiguration().ConfigurePipeline().AddRequirements().AddModules();
 
 await using var pipeline = await builder.BuildAsync();
 await pipeline.RunAsync();
