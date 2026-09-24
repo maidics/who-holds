7dc3f9216197946f7f9ff4250ef6a44425b43bb8
maidics <your-username@users.noreply.github.com>
2026-09-24 16:34:04 +0200

    chore: add IsTagPush extension to IConfiguration

---
+++ src/WhoHolds.Pipeline/Extensions/ConfigurationExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Configuration;
+using WhoHolds.Pipeline.Constants;
+
+namespace WhoHolds.Pipeline.Extensions;
+
+public static class ConfigurationExtensions
+{
+    extension(IConfiguration configuration)
+    {
+        public bool IsTagPush()
+        {
+            var refType = configuration.GetValue<string>(Repo.GitHubRefTypeEnvVar);
+
+            if (string.IsNullOrEmpty(refType))
+                throw new InvalidOperationException(
+                    $"Configuration not found: '{Repo.GitHubRefTypeEnvVar}'."
+                );
+
+            return refType == Repo.GitHubTagRef;
+        }
+    }
+}
