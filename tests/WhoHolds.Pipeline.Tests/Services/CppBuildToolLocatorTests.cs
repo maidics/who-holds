8269684be8011edf7e65819b69f13c7244ef4a0d
maidics <your-username@users.noreply.github.com>
2026-09-25 14:35:14 +0200

    fix(test): add missing csproj reference

--- tests/WhoHolds.Pipeline.Tests/Services/CppBuildToolLocatorTests.cs
+++ tests/WhoHolds.Pipeline.Tests/Services/CppBuildToolLocatorTests.cs
@@ -1,5 +1,10 @@
-﻿using WhoHolds.Pipeline.Constants;
+﻿using ModularPipelines.Context;
+using ModularPipelines.Context.Domains;
+using ModularPipelines.Context.Domains.Shell;
+using ModularPipelines.Models;
+using WhoHolds.Pipeline.Constants;
 using WhoHolds.Pipeline.Services;
+using WhoHolds.Tests.Shared;
 
 namespace WhoHolds.Pipeline.Tests.Services;
 
@@ -32,10 +37,40 @@ public sealed class CppBuildToolLocatorTests
     [Test]
     public async Task ShouldReturnPathWhenExists()
     {
-        var locator = new CppBuildToolLocator(CppBuildToolLocator.DefaultPath);
+        await using var fs = new TestFileSystem();
+        await fs.InitializeAsync();
 
-        var result = await locator.LocateAsync(null!);
+        var file = fs.CreateTestFile();
+
+        var locator = new CppBuildToolLocator(file);
+
+        const string stdout = nameof(stdout);
+
+        var command = ICommandContext.Mock();
+        command
+            .ExecuteCommandLineTool(Any(), Any(), Any())
+            .Returns(
+                new CommandResult(
+                    commandInput: Repo.VsWhere,
+                    workingDirectory: AppContext.BaseDirectory,
+                    standardOutput: stdout,
+                    standardError: string.Empty,
+                    environmentVariables: new Dictionary<string, string?>(),
+                    startTime: DateTimeOffset.UnixEpoch,
+                    endTime: DateTimeOffset.UnixEpoch,
+                    duration: TimeSpan.Zero,
+                    exitCode: 0
+                )
+            );
+
+        var shell = IShellContext.Mock();
+        shell.Command.Returns(command.Object);
+
+        var context = IPipelineContext.Mock();
+        context.Shell.Returns(shell.Object);
+
+        var result = await locator.LocateAsync(context);
         result.VsWhereFound.ShouldBeTrue();
-        result.InstallationPath.ShouldNotBeNullOrEmpty();
+        result.InstallationPath.ShouldBe(stdout);
     }
 }
